d6c422e545e851a86c758458878ff2e6132bdbda
Skwlx <pawelslawuta@MacBook-Pro-Pawe.local>
2020-05-03 22:04:49 +0200

    Console window added and some basic typing script.

--- src/js/components/machineWriting.js
+++ src/js/components/machineWriting.js
@@ -1,9 +1,14 @@
 class Writing{
     constructor(){
-
+        this.message = ["Witaj, nazywam się Paweł", "Junior front-end", "Web developer", "Student Informatyki"]
+        this.text = document.querySelector(".jumbotron__console-window__text");
+        this.wordIndex = 0;
     }
-    write(){
-        console.log("Pisze");
+    type(){
+        this.message.forEach(word => {
+            const letter = word[this.wordIndex];
+            this.text.textContent += letter;
+        });
     }
 }
 
--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,10 @@
 import Writing from './components/machineWriting';
 import Stars from './components/Stars';
 import "../sass/index.scss";
-const star = new Stars(document.querySelector("#canvas"));
-star.run();
+
+window.addEventListener('DOMContentLoaded', () => {
+    const star = new Stars(document.querySelector("#canvas"));
+    const write = new Writing();
+    write.type();
+    star.run();
+});
